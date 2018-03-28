8a49770293ee5116aa77dac960da324a6d684902
fuhang2000 <258103068@qq.com>
2018-03-28 21:03:55 +0800

    11
    22

--- src/main/java/com/imooc/VO/ProductVO.java
+++ src/main/java/com/imooc/VO/ProductVO.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Data
 public class ProductVO {
 
-@JsonProperty("name")
+    @JsonProperty("name")
     private  String  categroyName;
     @JsonProperty("type")
     private  Integer categroyType;
--- src/test/java/com/imooc/repository/ProductCategoryRepositoryTest.java
+++ src/test/java/com/imooc/repository/ProductCategoryRepositoryTest.java
@@ -66,13 +66,9 @@ public void  findBycatgoryTypeInTest(){
         List<ProductCategory> result=repository.findByCategoryTypeIn(list);
 
         Assert.assertNotEquals(0,result.size());
-
-<<<<<<< HEAD
     ProductCategory productCategory=repository.findOne(1);
     System.out.println("productCategory-------------------------");
     System.out.println("productCategory"+productCategory.toString());
-=======
->>>>>>> 1777036b221af46730090a211da8d112e352e07f
 }
 
 }
